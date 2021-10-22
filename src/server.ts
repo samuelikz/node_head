7ec516642070fbf3f7fe828c26c6698573cc0603
samuelikz <samuel.nds@outlook.com>
2021-10-21 21:44:21 -0300

    começando Front-end

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import { serverHttp } from "./app";
 
 
-serverHttp.listen(4000, () => console.log(`Server is running on port 4000`));
+serverHttp.listen(4400, () => console.log(`Server is running on port 4400`));
