df836e9784e950b4ac9c46563c7b1d1357bbebb9
samuelikz <samuel.nds@outlook.com>
2021-10-22 22:49:55 -0300

    Criando contexts

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,8 +1,45 @@
 import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+
+type AuthResponse = {
+    token: string;
+    user : {
+        id :string;
+        avatar_url: string;
+        login: string;
+    }    
+}
 
 export function LoginBox(){
     const singInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=7d0be3888e21a779b2ef`
+
+    async function signIn(githubCode: string){
+        const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        })
+
+        const {token, user} = response.data;
+
+        localStorage.setItem('@dowhile:token', token);
+
+        console.log(user)
+    }
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes('?code=')
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=')
+
+            window.history.pushState({}, '', urlWithoutCode)
+
+            signIn(githubCode)
+        }
+    })
+
     return(
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem </strong>
