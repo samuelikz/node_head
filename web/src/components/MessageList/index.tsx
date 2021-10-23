360e9cf8ee47da75935f81feb919d4fb4e422916
samuelikz <samuel.nds@outlook.com>
2021-10-22 21:00:13 -0300

    Componentes com API

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,46 @@
-export function MessageList(){
-    return(
-        <h1>MessageList</h1>
+import styles from './styles.module.scss';
+import logoImg from '../../assets/logo.svg'
+import { api } from '../../services/api'
+import { useEffect, useRef, useState } from 'react';
+
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
+
+export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/lest3').then(response => {
+            setMessages(response.data)
+        })
+    }, [])
+    return (
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="" />
+
+            <ul className={styles.messageList}>
+                {messages.map(message => {
+                    return (
+                        <li key={message.id} className={styles.message}>
+                            <p className={styles.messageContent}>
+                                {message.text}
+                            </p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
     )
 }
