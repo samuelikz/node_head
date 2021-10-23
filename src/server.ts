37509fa701aaf17dbc41237c01bbffbdc179d5e7
samuelikz <samuel.nds@outlook.com>
2021-10-22 21:31:50 -0300

    Realizada a comunicação de login com o backend

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import { serverHttp } from "./app";
 
 
-serverHttp.listen(4400, () => console.log(`Server is running on port 4400`));
+serverHttp.listen(4400, () => console.log(`Server is running on port http://localhost:4400`));
--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -2,10 +2,11 @@ import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc';
 
 export function LoginBox(){
+    const singInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=7d0be3888e21a779b2ef`
     return(
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem </strong>
-            <a href="#" className={styles.singInWithGithub}>
+            <a href={singInUrl} className={styles.singInWithGithub}>
                 <VscGithubInverted size="24"/>
                 Entrar com github
             </a>
